4d9f1eefc0d9cb24f513c77a50f0c3abb7d5cd87
iborhim008 <sunnat5809@gmail.com>
2026-05-15 18:39:21 +0500

    getting network IPv4

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,47 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import os from 'os'
+
+const getIPv4 = () => {
+  const interfaces = os.networkInterfaces()
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address
+      }
+    }
+  }
+  return 'localhost'
+}
+
+const localIP = getIPv4()
+const backendPort = 8000
+
 
 export default defineConfig({
+  host: '0.0.0.0',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   },
+  
   server: {
-    port: 5174,
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        // Agar brauzerda localhost:5173 dan kirilsa → localhost backend
+        // Agar boshqa qurilmadan kirilsa → network IP backend
+        target: `http://${localIP}:${backendPort}`,
         changeOrigin: true,
+        secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
-  }
+  },
 })
